806d89777f1f26118a045d4340c96d97af5d699f
Nina Nyberg <96874461+NinaNyberg@users.noreply.github.com>
2022-05-12 19:00:29 +0300

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,3 +5,16 @@ window.addEventListener(
   },
   false
 );
+
+let loadMoreBtn = document.querySelector('#load');
+let currentItem = 3;
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.container .row .col-4')];
+  for (let i = currentItem; i < currentItem + 3; i++) {
+    if (boxes[i]) boxes[i].style.display = 'flex';
+  }
+  currentItem += 3;
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = 'none';
+  }
+};
